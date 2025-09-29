f697f3be3015c571e6cd12c85cecc2ac521ca9dc
pi63on <94048525+pi63on@users.noreply.github.com>
2025-09-30 00:09:48 +0200

    proof of concept demo

--- script.js
+++ script.js
@@ -1,69 +1,51 @@
-// alert(6);
-// window.print();
-let currentId = "kukula";
-
 //  demo red rect
-d3.select("body")
-  .append("svg")
-    .attr("width", 500)
-    .attr("height", 500)
-      .append("g")
-        .attr("transform", "translate(100, 50), rotate(10)")
-    .append("rect")
-        .attr("width", 100)
-        .attr("height", 100)
-        .attr("fill", "red")
-        ;
+// let myRect = d3.select("body")
+//   .append("svg")
+//     .attr("width", 500)
+//     .attr("height", 500)
+//       .append("g")
+//         .attr("transform", "translate(100, 50), rotate(10)")
+//     .append("rect")
+//         .attr("width", 100)
+//         .attr("height", 100)
+//         .attr("fill", "red")
+//         ;
         
-        //Width and height
-        let w = 500;
-        let h = 500;
-        // Projection
-        let projection = d3.geo.mercator()
-        .center([17.109,48.132])
-        .translate([w/2,h/2])
-        .scale(h*2000);
+//Width and height
+let w = 0.5 * window.innerWidth;
+let h = 1 * window.innerHeight;
+
+// Projection
+let projection = d3.geo.mercator()
+.center([17.109,48.132])
+.translate([w/2,h/2])
+.scale(h*2000);
         
 //Create SVG
 let svg = d3.select("#svganchor")
     .append("svg")
+    .attr("id", "helo")
     .attr("width", w)
     .attr("height", h)
     ;
 
-// Add circles:
-d3.csv("data_cut.csv", (data) => {
-    // Add a scale for bubble size
-    //let valueExtent = d3.extent(data, (d) => { return +d.n; })
-    //let size = d3.scaleSqrt()
-    //    .domain(valueExtent)  // What's in the data
-    //    .range([1, 20])  // Size of circle in pixel
-
-    // let tooltip = d3.select("body")
-    //     .append("div")
-    //     .style("position", "absolute")
-    //     .style("text-align", "center")
-    //     .style("padding", "15px")
-    //     .style("font", "12px sans-serif")
-    //     .style("background", "white")
-    //     .style("border", "0px")
-    //     .style("border-radius", "8px")
-    //     .style("z-index", "10")
-    //     .style("visibility", "hidden")
-    //     .text("a simple tooltip");
+d3.csv("data_cut.csv", function(data){
+    // Add circles:
     svg
-        .selectAll("myCircles")
+        .selectAll("myCircle")
         .data(data)
         .enter()
         .append("circle")
+        .attr("class", "cercle")
 
         .attr("cx", (d) => { return projection([+d.X, +d.Y])[0] })
         .attr("cy", (d) => { return projection([+d.X, +d.Y])[1] })
 
         // radius of circle
-        .attr("r", (d) => { return 2 })
+        .attr("r", 2)
         // id of each circle
-        .attr("id", (d) => {return "s_"+d.video})
+        // .attr("id", (d) => {return "s_"+d.video})
+        .attr("id", function(d,i) {return "cir" + i})
         // style
         .attr("stroke-width", 0)
         .attr("fill", '#ffffffff')
@@ -72,33 +54,55 @@ d3.csv("data_cut.csv", (data) => {
         .on('mouseover', function (d, i) {
             d3.select(this).transition()
             .duration('200')
-            .attr("fill", "red");
+            .attr("fill", "orange");
         })
         .on('mouseout', function (d) {
             d3.select(this).transition()
             .duration('200')
-            .attr("fill", "white")
+            .attr("fill", "white");
         })
-        .on("click", function(d){
-            alert("hop klik");
-            currentId = d3.select(this).id;
-            console.log(currentId);
+        .on("click", function(d, i){
+            alert(i);
             })
-            // myPlayer.currentTime(d.video);})
-        ;
-    });
+        ; 
+    })
 
-Circelos = d3.selectAll("myCircles");
-console.log(Circelos)
+d3.csv("m1.csv", function(data){
+// Add circles:
+svg
+    .selectAll("myCircle2")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "cercle")
 
+    .attr("cx", (d) => { return projection([+d.X, +d.Y])[0] })
+    .attr("cy", (d) => { return projection([+d.X, +d.Y])[1] })
 
+    // radius of circle
+    .attr("r", 2)
+    // id of each circle
+    // .attr("id", (d) => {return "s_"+d.video})
+    .attr("id", function(d,i) {return "cir" + i})
+    // style
+    .attr("stroke-width", 0)
+    .attr("fill", '#ffffffff')
+    .attr("fill-opacity", 1)
+
+    .on('mouseover', function (d, i) {
+        d3.select(this).transition()
+        .duration('200')
+        .attr("fill", "orange");
+    })
+    .on('mouseout', function (d) {
+        d3.select(this).transition()
+        .duration('200')
+        .attr("fill", "white");
+    })
+    .on("click", function(d, i){
+        alert(i);
+        })
+    ; 
+})
 
     
-    // setInterval(function() {
-    //     video_where = player.getCurrentTime() || 0;
-    //     video_where = Math.round(video_where,0)
-    //     video_where_2 = "s_" + video_where
-    //     svg.selectAll('circle').attr('fill','#053b87')
-    //     svg.select("#" + video_where_2).attr("fill", 'black')
-    //     svg.select("#" + video_where_2).attr("fill-opacity", 1)
-    // }, 3000);
